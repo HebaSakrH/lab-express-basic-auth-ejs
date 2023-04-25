b1f309ed1f4521b2f427ef59b2c67cd992dded19
HebaSakrH <128267375+HebaSakrH@users.noreply.github.com>
2023-04-25 20:17:54 +0200

    done

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,21 @@
+const express = require('express')
+const { isLoggedIn } = require("../middleware/route-guard");
 const router = require("express").Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+
+router.get('/profile', (req, res, next) => {
+  res.render('profile', {user: req.session.user })
+})
+
+router.get('/logout', (req, res, next) => {
+  req.session.destroy(err => {
+    if(err) next(err)
+    res.redirect('/')
+  })
+})
 
 module.exports = router;
